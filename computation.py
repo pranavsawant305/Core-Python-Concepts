f5f424a810f44d1757218f602e4deacef35efb20
pranavsawant305 <pranavsawant305@gmail.com>
2024-07-03 19:22:13 +0530

    Add files via upload

---
+++ computation.py
@@ -0,0 +1,16 @@
+def add(n1,n2):
+    r=n1+n2
+    return r
+
+def sub(n1,n2):
+    r=n1+n2
+    return r
+
+def mult(n1,n2):
+    r=n1*n2
+    return r
+
+def div(n1,n2):
+    r=n1/n2
+    return r
+    
